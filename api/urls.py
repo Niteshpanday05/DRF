8b0a0550aad1616c85f1218bcd64ee8b4f4e3722
Niteshpanday05 <eagleseye.np@gmail.com>
2026-02-25 20:57:01 +0545

    employee detail

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('students/', views.studentsView),
     path('student/<int:pk>/', views.studentDetailView),
     
-    path('employees/', views.Employees.as_view())
+    path('employees/', views.Employees.as_view()),
+    path('employees/<int:pk>/', views.EmployeeDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from employees.models import Employee
+from django.http import Http404
 
 # Create your views here.
 
@@ -58,6 +59,18 @@ class Employees(APIView):
     def post(self,request):
         serializer = EmployeeSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(),
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
         
+    
+class EmployeeDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+        
+    def get(self,request,  pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
